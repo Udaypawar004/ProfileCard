2816eaf66f373c779f10257ee4484b5b810d4310
Udaypawar004 <111646210+Udaypawar004@users.noreply.github.com>
2022-12-06 17:53:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const showSocial = (toggleCard, socialCard) =>{
+    const toggle = document.getElementById(toggleCard),
+          social = document.getElementById(socialCard)
+
+    toggle.addEventListener('click', () =>{
+        if(social.classList.contains('animation')){
+            social.classList.add('down-animation')
+
+            setTimeout(() =>{
+                social.classList.remove('down-animation')
+            }, 1000)
+        }
+        social.classList.toggle('animation')
+    })
+}
+showSocial('card-toggle', 'card-social')
